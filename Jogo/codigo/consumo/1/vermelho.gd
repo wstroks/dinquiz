47c384a27ff5f0bc22505fb90b87b61c3ca320f3
Washington Pagotto Batista <wstroks@hotmail.com>
2019-09-04 21:40:39 -0300

    add

--- Jogo/codigo/consumo/1/vermelho.gd
+++ Jogo/codigo/consumo/1/vermelho.gd
@@ -3,17 +3,9 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var a=0
-var conta=0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    
-	a=new_script._getdin()
-	if(a==0):
-		$Label.set_text("68")
-	else:
-		conta=68-a
-		$Label.set_text(str(conta))
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,8 +14,5 @@ func _ready():
 
 
 func _on_TextureButton_pressed():
-	if(a==68):
-		get_tree().change_scene("res://codigo/consumo/1/vermelho.tscn")
-	else:
-		get_tree().change_scene("res://tela_apresenta.tscn")
+	get_tree().change_scene("res://tela_apresenta.tscn")
 	pass # Replace with function body.
